2cd42f65e7a1829f4dbac866cc1d74d0b2d3a1c5
unknown <asmanaveed1982@gmail.com>
2023-09-29 19:47:36 -0400

    my project

---
+++ HotelManagmentSystem.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+from LoginModule import LoginManager
+
+class Hotel:
+    def __init__(self):
+        print("Constructor called")
+        self.main_win = tk.Tk()
+        self.main_win.title("Hotel Room Management")
+        
+        # Set a fixed window size (e.g., 650x250)
+        self.main_win.geometry("650x250")
+        
+        # Make the window non-resizable
+        self.main_win.resizable(False, False)
+
+        # Configure columns if needed
+        self.main_win.columnconfigure(0, minsize=50)
+        self.main_win.columnconfigure(1, minsize=150)
+        self.main_win.columnconfigure(2, minsize=250)
+        self.main_win.columnconfigure(3, minsize=150)
+
+        #Creating Label of Main Window
+        self.title_label = tk.Label(text='Hotel Management System', font=("Helvetica", 15), fg="navy")
+        self.title_label.grid(row=0, column=1, columnspan=3)  # Center the label in columns 1, 2, and 3
+
+        self.create_account_button = tk.Button(text="Create Account", width=15, font=("Helvetica", 10), command=self.create_account)
+        self.create_account_button.grid(row=1, column=1, sticky="nsew")  # Position the "Create Account" button in column 1
+
+        self.login_button = tk.Button(text="Login", width=10, font=("Helvetica", 10), command=self.login)
+        self.login_button.grid(row=1, column=2, sticky="nsew")  # Position the "Login" button in column 2
+
+        self.quit_button = tk.Button(text="Cancel", width=10, font=("Helvetica", 10), command=self.main_win.destroy)
+        self.quit_button.grid(row=1, column=3, sticky="nsew")  # Position the "Cancel" button in column 3
+
+
+        # Load and display the image
+        img = Image.open("C:\\Users\\Asma\\Desktop\\Asma.gif")
+        img = img.resize(650, 250)
+        img = ImageTk.PhotoImage(img)
+        self.labelGIF = tk.Label(image=img)
+        self.labelGIF.image = img
+        self.labelGIF.place(x=50, y=60)
+
+        tk.mainloop()
+
+    def create_account(self):
+        # Replace this with the code to handle the "Create Account" button click
+        print("Create Account button clicked")
+
+    def login(self):
+        
+        # Create an instance of the LoginManager class
+        login_manager_instance = LoginManager()
+
+        # Set initial username and password (you can change these)
+        login_manager_instance.AddLogin("admin", "password")
+
+        # Run the login manager and display the login 
+        print("calling run")
+        #login_manager_instance.run()
+
+Hotel_Mnagement_System = Hotel()
---
+++ LoginModule.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+
+class LoginManager:
+    def __init__(self):
+        print("loginmanager constructor called")
+        self.usernameStored = ""
+        self.passwordStored = ""
+        self.AccountLogin = tk.Tk()  # Create a Tkinter AccountLogin window
+        self.AccountLogin.title("Account Login")
+        
+        # Set  window size
+        self.AccountLogin.geometry("400x200")
+        
+        # Create labels and entry widgets for username and password
+        tk.Label(self.AccountLogin, text="Username:").pack()
+        
+        self.username_entry = tk.Entry(self.AccountLogin)
+        self.username_entry.pack()
+        
+        tk.Label(self.AccountLogin, text="Password:").pack()
+        self.password_entry = tk.Entry(self.AccountLogin, show="*")  # Use show="*" to hide the password
+        self.password_entry.pack()
+        
+        # Create a login button
+        tk.Button(self.AccountLogin, text="Login", command=self.verify_login).pack()
+    
+    def verify_login(self):
+        # Get the username and password entered by the user
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+        
+        # Check if the entered credentials are valid
+        if self.VerifyLogin(username, password):
+            print("Login successful")
+        else:
+            print("Login failed")
+        
+    def VerifyLogin(self, username, password):
+        if username == self.usernameStored and password == self.passwordStored:
+            return True
+        else:
+            return False
+
+    def AddLogin(self, username, password):
+        self.usernameStored = username
+        self.passwordStored = password
+
+    def run(self):
+        # Start the Tkinter main loop
+        self.AccountLogin.mainloop()
+
+    
+
